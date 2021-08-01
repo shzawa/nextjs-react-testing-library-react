bb9b8e92e44bfa53741cde634835c108e9afee38
shzawa <ozawa.shotaro0114@gmail.com>
2021-08-02 00:03:19 +0900

    add: Blogページ&テストのパス

--- pages/blog-page.tsx
+++ pages/blog-page.tsx
@@ -1,10 +1,27 @@
+import { GetStaticProps } from 'next'
 import Layout from '../components/Layout'
+import { getAllPostsData } from '../lib/fetch'
+import Post from '../components/Post'
+import { POST } from '../types/Types'
 
-const BlogPage: React.FC = () => {
+interface STATIC_PROPS {
+  posts: POST[]
+}
+
+const BlogPage: React.FC<STATIC_PROPS> = ({ posts }) => {
   return (
     <Layout title="Blog">
-      <p className="text-4xl">blog page</p>
+      <p className="text-4xl mb-10">blog page</p>
+      <ul>{posts && posts.map((post) => <Post key={post.id} {...post} />)}</ul>
     </Layout>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPostsData()
+  return {
+    props: { posts },
+  }
+}
+
 export default BlogPage
